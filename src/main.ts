5e2c3b9dfafe970b2483f6d3d601d1248bda49e7
Zefir Kirilov <contact@zefir.pro>
2026-03-22 06:51:36 +0200

    fetch settings from /env.json

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,32 @@ import "./style.css";
 import { AppRoot } from "./components/AppRoot";
 import { WsClient } from "./api/WsClient";
 
-const api = await WsClient.connect("ws://localhost:14454");
+interface Env {
+  ALTMANAGER_SERVER: string;
+}
+
+async function fetchEnv() {
+  const defaultEnv: Env = {
+    ALTMANAGER_SERVER: "http://localhost:14454",
+  }
+  try {
+    const res = await fetch("/env.json");
+    if (!res.ok) {
+      return defaultEnv;
+    }
+    const env = await res.json();
+    return {
+      ALTMANAGER_SERVER: env?.ALTMANAGER_SERVER ?? defaultEnv.ALTMANAGER_SERVER,
+    }
+  } catch (e) {
+    console.error("Failed to fetch env.json", e);
+    return defaultEnv;
+  }
+}
+
+const config = await fetchEnv();
+
+const api = await WsClient.connect(config.ALTMANAGER_SERVER);
 const root = new AppRoot(api);
 
 document.body.append(root);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { minifyTemplateLiterals } from "rollup-plugin-minify-template-literals";
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    minifyTemplateLiterals(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+
+  return {
+    plugins: [
+      tailwindcss(),
+      minifyTemplateLiterals(),
+      {
+        name: "dev-env",
+        configureServer(server) {
+          server.middlewares.use("/env.json", (_, res) => {
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(env));
+          });
+        },
+      },
+    ],
+  };
 });
